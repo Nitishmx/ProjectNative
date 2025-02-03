52b6698978c486fe3a8e3d8bb86b4e9478bbda9c
NITISH <145883497+Nitishmx@users.noreply.github.com>
2025-02-03 14:40:52 +0530

    restaurant page done

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { FlatList, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import Welcome from './components/Welcome'
 import Home from './components/Home'
@@ -8,19 +8,16 @@ import Verification from './components/Verification'
 import {NavigationContainer} from "@react-navigation/native"
 import {createNativeStackNavigator} from "@react-navigation/native-stack"
 import { Colors } from 'react-native/Libraries/NewAppScreen'
+import Delaware from './components/Delaware'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 const Stack=createNativeStackNavigator()
+const Tap=createBottomTabNavigator()
+
 const App = () => {
   return (
-    // <View>
-    //   {/* <Welcome/> */}
-    //   {/* <Home/> */}
-    //   {/* <DeliverOrder/> */}
-    //   {/* <Account/> */}
-    //   {/* <Verification/> */}
-    // </View>
     <NavigationContainer>
         <Stack.Navigator>
-            <Stack.Screen name="starting" component={Welcome}/>
+            <Stack.Screen name="ff" component={Welcome} options={{title:"",headerShown:false}}/>
             <Stack.Screen name="university" component={Home} options={{headerStyle:{
               backgroundColor:"#009EEB"
             },headerTintColor:"white",title:""}}/>
@@ -33,9 +30,9 @@ const App = () => {
             <Stack.Screen name="verification" component={Verification} options={{headerStyle:{
               backgroundColor:"#009EEB"
             },headerTintColor:"white",title:""}}/>
+            <Stack.Screen name="home" component={Delaware} options={{headerShown:false}}/>    
         </Stack.Navigator>
     </NavigationContainer>
-
   )
 }
 
